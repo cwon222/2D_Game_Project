e53a9070d0578c4dfaa773248b83d87cbc71b488
qoxowjd <qoxowjd77@naver.com>
2024-06-20 14:05:42 +0900

    건물 베이스 체력바 생성

--- 2D_URP_TowerDefense/Assets/Scripts/Controll/BlueBase.cs
+++ 2D_URP_TowerDefense/Assets/Scripts/Controll/BlueBase.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.UI;
 
 public class BlueBase : MonoBehaviour
 {
-    public int health;
+    public int maxHealth = 100;
+    int health;
+    public int Health
+    {
+        get => health;
+        set => health = Mathf.Clamp(value, 0, health);
+    }
     public TMP_Text baseCountText;
     public GameObject gameOverScene;
-    
+    public Slider slider;
 
     // Start is called before the first frame update
     void Start()
     {
-        health = 100;
+        health = maxHealth;
+        SetMaxHealth(health);
         baseCountText.text = health.ToString();
         gameOverScene.SetActive(false);
     }
@@ -29,24 +37,33 @@ public class BlueBase : MonoBehaviour
         }
     }
 
+    public void SetMaxHealth(int hp)
+    {
+        slider.maxValue = hp;
+        slider.value = hp;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("EnemyU1Weapon"))
         {
             health--;
             health = Mathf.Clamp(health, 0, 100);
+            slider.value = health;
             baseCountText.text = health.ToString();
         }
         if (collision.gameObject.CompareTag("EnemyU2Weapon"))
         {
             health--;
             health = Mathf.Clamp(health, 0, 100);
+            slider.value = health;
             baseCountText.text = health.ToString();
         }
         if (collision.gameObject.CompareTag("EnemyU3Weapon"))
         {
             health -= 3;
             health = Mathf.Clamp(health, 0, 100);
+            slider.value = health;
             baseCountText.text = health.ToString();
         }
     }
--- 2D_URP_TowerDefense/Assets/Scripts/Controll/RedBase.cs
+++ 2D_URP_TowerDefense/Assets/Scripts/Controll/RedBase.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.UI;
 
 public class RedBase : MonoBehaviour
 {
-    public int health;
+    public int maxHealth = 100;
+    int health;
+    public int Health
+    {
+        get => health;
+        set => health = Mathf.Clamp(value, 0, health);
+    }
     public TMP_Text baseCountText;
     public GameObject gameOverScene;
-    
+    public Slider slider;
+
+    private void Awake()
+    {
+        
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        health = 1;
+        health = maxHealth;
+        SetMaxHealth(health);
         baseCountText.text = health.ToString();
         gameOverScene.SetActive(false);
     }
@@ -29,24 +42,33 @@ public class RedBase : MonoBehaviour
         }
     }
 
+    public void SetMaxHealth(int hp)
+    {
+        slider.maxValue = hp;
+        slider.value = hp;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("GoodU1Weapon"))
         {
             health--;
             health = Mathf.Clamp(health, 0, 100);
+            slider.value = Health;
             baseCountText.text = health.ToString();
         }
         if (collision.gameObject.CompareTag("GoodU2Weapon"))
         {
             health--;
             health = Mathf.Clamp(health, 0, 100);
+            slider.value = Health;
             baseCountText.text = health.ToString();
         }
         if (collision.gameObject.CompareTag("GoodU3Weapon"))
         {
             health -= 3;
             health = Mathf.Clamp(health, 0, 100);
+            slider.value = Health;
             baseCountText.text = health.ToString();
         }
     }
