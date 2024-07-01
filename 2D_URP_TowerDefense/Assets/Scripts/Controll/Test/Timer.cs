989a30aaf94844f153da75d00796be4b9afe5ace
qoxowjd <qoxowjd77@naver.com>
2024-07-01 14:54:28 +0900

    타이머 스크립트 작성중

--- 2D_URP_TowerDefense/Assets/Scripts/Controll/Test/Timer.cs
+++ 2D_URP_TowerDefense/Assets/Scripts/Controll/Test/Timer.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float timer;
+
+    float remainTime;
+
+    TextMeshProUGUI timerText;
+
+    private void Awake()
     {
-        
+        remainTime = timer;
+        timerText = GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        remainTime -= Time.deltaTime;
     }
+
+    void TimeText()
+    {
+        timerText.text = remainTime.ToString();   
+    }
+
 }
